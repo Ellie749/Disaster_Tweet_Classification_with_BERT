5a1c6e38f9330e4b61e54c399b70691070585766
Ellie <elham.azizi749@gmail.com>
2024-12-10 23:35:44 -0500

    Project initialization

---
+++ src/dataset/tokenizer.py
@@ -0,0 +1,27 @@
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+from transformers import AutoTokenizer
+
+
+def read_data(train_df_path: str, test_df_path: str):
+    train_dataframe = pd.read_csv(train_df_path, usecols=['text', 'target'])
+    test_dataframe = pd.read_csv(test_df_path, usecols=['text'])
+
+    #print(train_dataframe)
+    #print(test_dataframe)
+
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+    X_train = tokenizer(train_dataframe.text.tolist(), padding=True, max_length=36, truncation=True, return_tensors='tf')
+    X_test = tokenizer(test_dataframe.text.tolist(), padding=True, max_length=36, truncation=True, return_tensors='tf')
+    y_train = test_dataframe.text.tolist()
+    y_test = None
+    
+    print(np.array(X_train).shape)
+    print(np.array(X_test).shape)
+    print(np.array(y_train).shape)
+
+    return X_train, X_test, y_train, y_test
+
+read_data('train.csv', 'test.csv')
